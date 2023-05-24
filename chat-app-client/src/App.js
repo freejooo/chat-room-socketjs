0aae370223bcc27907e370b027ff3fbe91fcc3ca
Meet Bhanushali <iitjeemeet@gmail.com>
2023-05-24 07:54:02 +0530

    add active section

--- chat-app-client/src/App.js
+++ chat-app-client/src/App.js
@@ -1,30 +1,62 @@
 import React, { useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
-import "./App.css";
+import './App.css';
 
-const socket = io('http://localhost:3001');
+const socket = io('http://localhost:3002');
 
 function App() {
+  const [username, setUsername] = useState('');
   const [messages, setMessages] = useState([]);
   const [inputMessage, setInputMessage] = useState('');
-  const [username, setUsername] = useState('');
+  const [activeUsers, setActiveUsers] = useState([]);
+  const [userHistory, setUserHistory] = useState([]);
   const messageListRef = useRef(null);
+  const [chatStarted, setChatStarted] = useState(false);
+  const [loadingMessages, setLoadingMessages] = useState(false);
+
+  const handleStartChat = () => {
+    if (username) {
+      setChatStarted(true);
+      socket.connect();
+      socket.emit('join', username);
+    }
+  };
 
   useEffect(() => {
-    socket.on('chat message', message => {
-      setMessages(prevMessages => [...prevMessages, message]);
-      scrollToBottom();
-    });
+    if (chatStarted) {
+      socket.on('connect', () => {
+        console.log('Connected to server');
+      });
 
-    return () => {
-      socket.disconnect();
-    };
-  }, []);
+      socket.on('chat message', (message) => {
+        setMessages((prevMessages) => [...prevMessages, message]);
+        scrollToBottom();
+      });
+
+      socket.on('active users', (users) => {
+        setActiveUsers(users);
+      });
+
+      socket.on('user history', (history) => {
+        setUserHistory(history);
+      });
+
+      socket.on('disconnect', () => {
+        console.log('Disconnected from server');
+        setActiveUsers([]);
+      });
+
+      return () => {
+        socket.disconnect();
+      };
+    }
+  }, [chatStarted]);
 
   useEffect(() => {
     const fetchMessages = async () => {
+      setLoadingMessages(true);
       try {
-        const response = await fetch('http://localhost:3001/messages');
+        const response = await fetch('http://localhost:3002/messages');
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
@@ -33,18 +65,22 @@ function App() {
         scrollToBottom();
       } catch (error) {
         console.error('Fetching messages failed: ', error);
+      } finally {
+        setLoadingMessages(false);
       }
     };
 
-    fetchMessages();
-  }, []);
+    if (chatStarted) {
+      fetchMessages();
+    }
+  }, [chatStarted]);
 
   const scrollToBottom = () => {
     messageListRef.current.scrollTop = messageListRef.current.scrollHeight;
   };
 
   const handleSendMessage = () => {
-    if (inputMessage && username) {
+    if (inputMessage) {
       const message = {
         username,
         message: inputMessage,
@@ -58,37 +94,73 @@ function App() {
   };
 
   return (
-    <div>
-      <h1>Chat App</h1>
-
-      <div className="chat-container">
-        <div className="message-list" ref={messageListRef}>
-          {messages.map((message, index) => (
-            <div key={index} className="message">
-              <span className="message-username">{message.username}:</span>{' '}
-              <span className="message-content">{message.message}</span>
-            </div>
-          ))}
-        </div>
-
-        <div className="input-container">
-          <input
-            type="text"
-            value={inputMessage}
-            onChange={e => setInputMessage(e.target.value)}
-            placeholder="Type your message..."
-          />
-
+    <div className="app">
+      {!chatStarted ? (
+        <div className="start-chat">
+          <h1>Enter your username</h1>
           <input
             type="text"
             value={username}
-            onChange={e => setUsername(e.target.value)}
+            onChange={(e) => setUsername(e.target.value)}
             placeholder="Your username..."
           />
-
-          <button onClick={handleSendMessage}>Send</button>
+          <button onClick={handleStartChat}>Start Chat</button>
         </div>
-      </div>
+      ) : (
+        <div className="chat-container">
+          <div className="sidebar">
+            <h2>Active Users</h2>
+            {activeUsers.length > 0 ? (
+              <ul>
+                {activeUsers.map((user) => (
+                  <li key={user}>{user}</li>
+                ))}
+              </ul>
+            ) : (
+              <p>No active users</p>
+            )}
+          </div>
+          <div className="chat">
+            <div className="message-list" ref={messageListRef}>
+              {loadingMessages ? (
+                <p>Loading messages...</p>
+              ) : messages.length > 0 ? (
+                messages.map((message) => (
+                  <div key={message._id} className="message">
+                    <span className="message-username">{message.username}:</span>{' '}
+                    <span className="message-content">{message.message}</span>
+                  </div>
+                ))
+              ) : (
+                <p>No messages</p>
+              )}
+            </div>
+            <div className="input-container">
+              <input
+                type="text"
+                value={inputMessage}
+                onChange={(e) => setInputMessage(e.target.value)}
+                placeholder="Type your message..."
+              />
+              <button onClick={handleSendMessage}>Send</button>
+            </div>
+          </div>
+          <div className="user-history">
+            <h2>User History</h2>
+            {userHistory.length > 0 ? (
+              <ul>
+                {userHistory.map((user, index) => (
+                  <li key={index}>
+                    {user.username} - {user.status}
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>No user history</p>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 }
--- chat-app-server/server.js
+++ chat-app-server/server.js
@@ -1,81 +1,96 @@
-// server.js
-
-// server.js
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
 const mongoose = require('mongoose');
-const cors = require('cors'); // Make sure to import cors
+const cors = require('cors');
 
-// Initialize the Express app and create an HTTP server
 const app = express();
-app.use(cors()); // Add this line to use cors
+app.use(cors());
 
 const server = http.createServer(app);
 
-// Initialize Socket.IO and attach it to the server with CORS configuration
 const io = socketIO(server, {
   cors: {
-    origin: "http://localhost:3000", // Allow this origin
-    methods: ["GET", "POST"], // Allow these methods
-    credentials: true // Allow credentials
-  }
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
 });
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/chat-app', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose
+  .connect('mongodb://localhost/chat-app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Error connecting to MongoDB:', err));
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
-// Define a ChatMessage schema and model in Mongoose
 const chatMessageSchema = new mongoose.Schema({
   username: String,
   message: String,
-  timestamp: { type: Date, default: Date.now }
+  timestamp: { type: Date, default: Date.now },
 });
 const ChatMessage = mongoose.model('ChatMessage', chatMessageSchema);
 
-// Socket.IO logic
-io.on('connection', socket => {
-  console.log('A user connected');
+const activeUsers = new Set();
+const userHistory = [];
 
-  // Listen for chat messages
-  socket.on('chat message', async data => {
-    // Save the chat message to MongoDB
-    
+io.on('connection', (socket) => {
+  let username;
+
+  socket.on('join', (user) => {
+    username = user;
+    socket.join('chat');
+    activeUsers.add(username);
+    io.to('chat').emit('active users', Array.from(activeUsers));
+    console.log(`${username} user connected`);
+
+    userHistory.push({ username, status: 'connected' });
+    io.emit('user history', userHistory);
+  });
+
+  socket.on('disconnect', () => {
+    if (username) {
+      activeUsers.delete(username);
+      io.to('chat').emit('active users', Array.from(activeUsers));
+      console.log(`${username} user disconnected`);
+
+      userHistory.push({ username, status: 'disconnected' });
+      io.emit('user history', userHistory);
+    }
+  });
+
+  socket.on('chat message', async (data) => {
     const chatMessage = new ChatMessage(data);
-    await chatMessage.save();  
+    await chatMessage.save();
     const savedMessage = await chatMessage.save();
     console.log('Message saved to MongoDB:', savedMessage);
 
-    // Convert the timestamp to a string
     const chatMessageData = savedMessage.toObject();
     chatMessageData.timestamp = chatMessageData.timestamp.toString();
 
-    // Broadcast the chat message to all connected clients
     console.log('Emitting message:', chatMessageData);
     io.emit('chat message', chatMessageData);
   });
-
-  // Handle disconnections
-  socket.on('disconnect', () => {
-    console.log('A user disconnected');
-  });
 });
 
-// Route for fetching old messages
-// Update the route for fetching old messages
 app.get('/messages', async (req, res) => {
-    const messages = await ChatMessage.find().sort({ timestamp: -1 }).limit(10);
-    res.json(messages.reverse());
-  });
-  
+  const messages = await ChatMessage.find()
+    .sort({ timestamp: -1 })
+    .limit(10);
+  res.json(messages.reverse());
+});
 
-// Start the server
-const port = 3001;
+app.get('/users', (req, res) => {
+  const users = Array.from(activeUsers);
+  res.json(users);
+});
+
+app.get('/user-history', (req, res) => {
+  res.json(userHistory);
+});
+
+const port = 3002;
 server.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
